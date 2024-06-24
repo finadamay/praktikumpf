5a2f586b06c2ef4292b16d36cc105119f7ce314d
finadamay <finadamay11@gmail.com>
2024-06-24 16:15:52 +0700

    RestAPI

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -36,6 +37,34 @@ class AuthController extends Controller
         'success' => true,
         'message' => 'Sukses register',
         'data' => $success
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ada kesalahan',
+                'data' => $validator->errors()
+            ]);
+        }
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau password salah'
+            ]);
+        }
+        $user = Auth::user();
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+        return response()->json([
+            'success' => true,
+            'message' => 'Sukses login',
+            'data' => $success
         ]);
     }
 }
